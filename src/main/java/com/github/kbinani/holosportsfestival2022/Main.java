367c0049a6a1f01683c14a281055e1efc90c8416
kbinani <kbinani.bt@gmail.com>
2022-10-31 03:16:27 +0900

    プラグイン本体は Listener にならなくて OK

--- src/main/java/com/github/kbinani/holosportsfestival2022/Main.java
+++ src/main/java/com/github/kbinani/holosportsfestival2022/Main.java
@@ -4,7 +4,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
   private MobFightEventListener mobFightEventListener;
   private FencingEventListener fencingEventListener;
 
