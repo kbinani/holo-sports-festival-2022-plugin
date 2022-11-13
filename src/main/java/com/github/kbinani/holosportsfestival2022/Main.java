f902061bdaa3455e22ec29752b846f45e7548109
kbinani <kbinani.bt@gmail.com>
2022-11-14 00:19:56 +0900

    通常の mob 湧きを無効化

--- src/main/java/com/github/kbinani/holosportsfestival2022/Main.java
+++ src/main/java/com/github/kbinani/holosportsfestival2022/Main.java
@@ -3,6 +3,9 @@ package com.github.kbinani.holosportsfestival2022;
 import com.github.kbinani.holosportsfestival2022.mob.MobFightEventListener;
 import org.bukkit.GameRule;
 import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,45 +13,62 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class Main extends JavaPlugin {
-  private MobFightEventListener mobFightEventListener;
-  private FencingEventListener fencingEventListener;
-  private BoatRaceEventListener boatRaceEventListener;
-  private RelayEventListener relayEventListener;
+public class Main extends JavaPlugin implements Listener {
+    private MobFightEventListener mobFightEventListener;
+    private FencingEventListener fencingEventListener;
+    private BoatRaceEventListener boatRaceEventListener;
+    private RelayEventListener relayEventListener;
 
-  public Main() {
-  }
-
-  @Override
-  public void onEnable() {
-    List<String> reasons = new ArrayList<>();
-    overworld().ifPresent(world -> {
-      Boolean mobGriefing = world.getGameRuleValue(GameRule.MOB_GRIEFING);
-      if (mobGriefing != null && mobGriefing) {
-        reasons.add( "mobGriefing gamerule is set to true");
-      }
-    });
-    if (!reasons.isEmpty()) {
-      getLogger().warning("Disabling the plugin because:");
-      for (String reason : reasons) {
-        getLogger().warning("  " + reason);
-      }
-      setEnabled(false);
-      return;
+    public Main() {
     }
 
-    this.mobFightEventListener = new MobFightEventListener(this);
-    this.fencingEventListener = new FencingEventListener(this);
-    this.boatRaceEventListener = new BoatRaceEventListener(this);
-    this.relayEventListener = new RelayEventListener(this);
-    PluginManager pluginManager = getServer().getPluginManager();
-    pluginManager.registerEvents(this.mobFightEventListener, this);
-    pluginManager.registerEvents(this.fencingEventListener, this);
-    pluginManager.registerEvents(this.boatRaceEventListener, this);
-    pluginManager.registerEvents(this.relayEventListener, this);
-  }
+    @Override
+    public void onEnable() {
+        List<String> reasons = new ArrayList<>();
+        overworld().ifPresent(world -> {
+            Boolean mobGriefing = world.getGameRuleValue(GameRule.MOB_GRIEFING);
+            if (mobGriefing != null && mobGriefing) {
+                reasons.add("mobGriefing gamerule is set to true");
+            }
+        });
+        if (!reasons.isEmpty()) {
+            getLogger().warning("Disabling the plugin because:");
+            for (String reason : reasons) {
+                getLogger().warning("  " + reason);
+            }
+            setEnabled(false);
+            return;
+        }
 
-  private Optional<World> overworld() {
-    return getServer().getWorlds().stream().filter(it -> it.getEnvironment() == World.Environment.NORMAL).findFirst();
-  }
+        this.mobFightEventListener = new MobFightEventListener(this);
+        this.fencingEventListener = new FencingEventListener(this);
+        this.boatRaceEventListener = new BoatRaceEventListener(this);
+        this.relayEventListener = new RelayEventListener(this);
+        PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(this.mobFightEventListener, this);
+        pluginManager.registerEvents(this.fencingEventListener, this);
+        pluginManager.registerEvents(this.boatRaceEventListener, this);
+        pluginManager.registerEvents(this.relayEventListener, this);
+        pluginManager.registerEvents(this, this);
+    }
+
+    @EventHandler
+    @SuppressWarnings("unused")
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        switch (e.getSpawnReason()) {
+            case NATURAL:
+            case VILLAGE_INVASION:
+            case BUILD_WITHER:
+            case BUILD_IRONGOLEM:
+            case BUILD_SNOWMAN:
+            case SPAWNER_EGG:
+            case SPAWNER:
+                e.setCancelled(true);
+                break;
+        }
+    }
+
+    private Optional<World> overworld() {
+        return getServer().getWorlds().stream().filter(it -> it.getEnvironment() == World.Environment.NORMAL).findFirst();
+    }
 }
